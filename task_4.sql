f96334e34f93a74b78949adecc9c17883ccc7d04
Samukelwe <samungalonde@gmail.com>
2025-02-02 21:23:30 +0200

    Task 4

---
+++ task_4.sql
@@ -0,0 +1,4 @@
+-- SQL script to print the full description of the books table
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
