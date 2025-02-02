5ebfa14dc7a89a83dcd326aea73915662d0b80ac
Samukelwe <samungalonde@gmail.com>
2025-02-02 21:19:41 +0200

    Task 3

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,4 @@
 -- SQL script to list all tables in the alx_book_store database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'alx_book_store';
+
+USE alx_book_store;
+SHOW TABLES;
